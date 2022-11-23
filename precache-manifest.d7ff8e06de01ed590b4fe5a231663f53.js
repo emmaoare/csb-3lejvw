7adf6e4901b46dc858b690c3c5b4b40362d94b57
Code Sandbox <hello@codesandbox.io>
2022-11-23 09:55:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d7ff8e06de01ed590b4fe5a231663f53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41c85eede0487d011d1def3beaf5e0d",
+    "url": "https://emmaoare.github.io/csb-3lejvw/index.html"
+  },
+  {
+    "revision": "b3a9783692d4c4072591",
+    "url": "https://emmaoare.github.io/csb-3lejvw/static/js/2.be363745.chunk.js"
+  },
+  {
+    "revision": "012329592aee30411133",
+    "url": "https://emmaoare.github.io/csb-3lejvw/static/js/main.d8417258.chunk.js"
+  },
+  {
+    "revision": "c1b95727d6dc53660076",
+    "url": "https://emmaoare.github.io/csb-3lejvw/static/js/runtime-main.c11cb8c1.js"
+  }
+]);
